6fe4df20fd418232471d718e722ae5d0e64b3b84
Yoshira Skarleth Barros Bernal <barrosysohira@gmail.com>
2025-06-17 17:55:45 -0500

    feat:guardar cambias de hmtl y css

---
+++ src/JS/main.js
@@ -0,0 +1,14 @@
+alert('¡Bienvenido al Clan Caimán! Juntos somos más fuertes');
+const mensajes = [
+    "¡Eres capaz de lograr todo lo que te propongas!",
+    "¡El código es tu superpoder! ",
+    "Cada error es una oportunidad para aprender.",
+    "El Clan Caimán te respalda, ¡sigue adelante!",
+    "No estás solo, estamos juntos en esto. ",
+    "El conocimiento se comparte, no se guarda. "
+  ];
+
+function mostrarMensaje() {
+    const mensaje = mensajes[Math.floor(Math.random() * mensajes.length)];
+    document.getElementById("mensajeMotivador").innerText = mensaje;
+  }
